bf559ffcef95ad99b0c0e18a3d63ecd81889b88d
kb172 <kb172@students.waikato.ac.nz>
2021-06-10 15:21:08 +1200

    Attempt 1 at reworking Tony's code (That is making it to the repo).

--- src/REcompile.java
+++ src/REcompile.java
@@ -43,9 +43,7 @@ public class REcompile {
 
         }
 
-        //---------------------------------------------------- Methods -------------------------------------------------
-
-        //
+        //---------------------------------------------------- Get-Set -------------------------------------------------
 
         //Gets the state number.
         public int getStateNumber() {
@@ -87,7 +85,106 @@ public class REcompile {
             this.branchTwo = branchTwo;
         }
 
+        //---------------------------------------------------- Methods -------------------------------------------------
+
         //
+        public String findExpression(String[] expression, int startingLocation)
+        {
+            int r;
+
+            r = findTerm();
+
+            if(isVocab(expression[startingLocation]) || expression[startingLocation]=='[')
+                findExpression();
+
+            return(r);
+
+        }
+
+        //
+        private String findTerm(String[] expression, int startingLocation
+        {
+            int r, t1, t2, f;
+
+            f = state - 1;
+
+            r = t1 = findFactor();
+
+            if(expression[startingLocation]=='*')
+            {
+                set_state(state,' ',state+1,t1);
+                j++;
+                r = state;
+                state++;
+            }
+
+            if(expression[startingLocation]=='+')
+            {
+                if(next1[f] == next2[f])
+                    next2[f] = state;
+
+                next1[f] = state;
+
+                f = state - 1;
+
+                j++;
+                r = state;
+                state++;
+
+                t2 = term();
+
+                set_state(r,' ',t1,t2);
+
+                if(next1[f] == next2[f])
+                    next2[f] = state;
+
+                next1[f] = state;
+
+            }
+
+            return(r);
+
+        }
+
+        //
+        private String findFactor(String[] expression, int startingLocation)
+        {
+            int r;
+
+            if(isvocab(expression[startingLocation]))
+            {
+                set_state(state,p[j],state+1,state+1);
+
+                j++;
+                r = state;
+                state++;
+
+            }
+            else
+                if(expression[startingLocation]=='[')
+                {
+                    j++;
+                    r = expression();
+
+                    if(expression[startingLocation]==']')
+                        j++;
+                    else
+                        error();
+
+            }
+            else
+                error();
+
+            return(r);
+
+        }
+
+        //
+        private boolean isVocab(String character)
+        {
+
+
+        }
 
         //Overrides the toString method for easy formatting.
         @Override
@@ -99,7 +196,7 @@ public class REcompile {
 
     //------------------------------------------ End of the ExpressionRecord class -------------------------------------
 
-    //---------------------------------------------------- REcomplie class ----------------------------------------------
+    //---------------------------------------------------- REcomplie class ---------------------------------------------
 
     //
 
@@ -129,17 +226,54 @@ public class REcompile {
             //Splits the expression into an array so they can be processed.
             String[] splitExpression = regularExpression.split("", 2147483647);
 
+            //Storage for the expressions being worked on.
+            String currentExpression, nextExpression = "";
+
+            //Where the broken expression is reformed.
+            String reformedExpression = "";
+
             //Loop through the expression to check its validity.
             for(int i = 0; i <= splitExpression.length; i++)
             {
+//                //Counts open/closed parentheses in the regular expression.
+//                int open, close;
+//
+//                //Counts how many of each parentheses there are.
+//                for (int j = 0; j <= splitExpression.length; j++)
+//                {
+//                    //Checks to see if the bracket is escaped.
+//                    if (c == '\\')
+//                    {
+//                        j++;
+//
+//                    }
+//                    else
+//                    {
+//                        if (c == "(")
+//                            open++;
+//
+//                        if (c == ")")
+//                            close++;
+//                    }
+//
+//                }
+//
+//                //If the parentheses are uneven then there is no point to see if they are correctly formated, because they can not be.
+//                if (open.equals(close) == false)
+//                {
+//                    System.err.println("Error: Unbalanced parentheses within the regular expression.");
+//                    break;
+//
+//                }
+
                 //
                 int stateNumber = i++;
 
                 //
-                char currentExpression = splitExpression[i];
+                currentExpression = splitExpression[i];
 
                 //
-                char nextExpression = splitExpression[i++];
+                nextExpression = splitExpression[i++];
 
                 //
                 switch(currentExpression)
@@ -172,12 +306,6 @@ public class REcompile {
             //Adds the 0's state to the FSM.
             FSMOutput.add(new ExpressionRecord(0, "", 1, -1));
 
-            //Storage for the expressions being worked on.
-            String currentExpression, nextExpression = "";
-
-            //Where the broken expression is reformed.
-            String reformedExpression = "";
-
             //Keeps track if the last character seen was an \
             boolean escaped = false;
 
@@ -188,62 +316,18 @@ public class REcompile {
                 int stateNumber = i++;
 
                 //
-                char currentExpression = splitExpression[i];
+                ExpressionRecord currentExpression;
 
                 //
-                char nextExpression = splitExpression[i++];
-
-                //
-                if(escaped = false) {
-                    //Checks for the proceding character.
-                    switch (currentExpression) {
-                        case ('\\'): {
-                            escaped = true;
-
-                            break;
-
-                        }
-                        case ('('): {
-
-                            break;
-
-                        }
-                        case ('{'): {
-
-                            break;
-
-                        }
-
-                    }
-
-                    //Checks for the post-character character.
-                    switch (nextExpression) {
-                        case ('*'): {
-
-                            break;
-
-                        }
-                        case ('+'): {
-
-                            break;
-
-                        }
-                        case ('?'): {
-
-                            break;
-
-                        }
-                        case ('|'): {
-
-                            break;
-
-                        }
-
-                    }
+                if(escaped = false)
+                {
+                    currentExpression.findExpression(splitExpression, i)
 
                 }
                 else
                 {
+                    currentExpression.findExpression(splitExpression, i)
+
                     escaped = false;
 
                 }
